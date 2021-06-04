010810f44779a3b559691632ce54adc2f1483b2a
jangcla <jangcla@ymail.com>
2021-06-03 17:09:32 -0700

    Finished initialize for Towers of Hanoi

--- W4D4/lib/TDD.rb
+++ W4D4/lib/TDD.rb
@@ -34,3 +34,42 @@ def my_transpose(array)
     end
     zipped
 end
+
+
+
+def stock_picker(arr)
+    max = 0
+    max_pair = nil
+    hash = {}
+
+    arr.each.with_index do |num1, i1|
+        arr.each.with_index do |num2, i2|
+            if i1 < i2 
+                hash[[i1, i2]] = num2 - num1
+            end
+        end
+    end
+
+    sorted = hash.sort_by {|k, v| v}
+    sorted[-1][0]
+end
+
+
+
+class Towers_of_Hanoi
+
+    attr_reader :towers, :number
+
+    def initialize(number)
+        @number = number
+        @towers = Array.new(number) {Array.new}
+        @towers[0] = (1..number).to_a
+    end
+
+
+
+
+
+
+end
+
--- W4D4/spec/tdd_spec.rb
+++ W4D4/spec/tdd_spec.rb
@@ -19,3 +19,31 @@ describe "my_transpose" do
         expect(my_transpose([[0, 1, 2], [3, 4, 5], [6, 7, 8]])).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
     end
 end
+
+
+describe "stock_picker" do
+    it "outputs the most profitable pair of days" do 
+        #stock picker has an array of [0,1,3,-1,4,-2,7] and index is the day that you bought the stock    
+        expect(stock_picker([0,1,3,2,1,4,7])).to eq([0, 6])
+    end
+
+end
+
+
+describe "Towers_of_Hanoi" do
+    let(:tdd) {Towers_of_Hanoi.new(3)}
+
+    describe "#initialize" do
+        it "should accept a number as arguement" do 
+           tdd
+        end
+
+        it "should create a 2D array with the range of 1 to num in the first array" do
+            expect(Towers_of_Hanoi.new(3).towers).to eq([ [1,2,3], [], [] ])
+        end
+    end
+
+
+
+
+end
