6a0dd3c1ad694872e15c35f5e3f7aa52ad9ad370
Jenny Nhan <j3nnynhan@gmail.com>
2021-06-16 15:42:14 -0700

    edited users controller file

--- W6D3/routes_and_controllers/app/controllers/users_controller.rb
+++ W6D3/routes_and_controllers/app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
     def index
+        render plain: "I'm in the index action!"
     end
+
+    def create
+        user = User.new(user_params)
+    if user.save!
+        render json: user
+    else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+    end
+
+    def show
+        render json: params
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
 end
