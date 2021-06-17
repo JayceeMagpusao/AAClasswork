c0a1a3e85401469433dd89d543b2837f75b15bc6
jaycee <jay27mag@yahoo.com>
2021-06-17 11:03:59 -0700

    pushing through artproj file

---
+++ W6D4/artsproj/app/models/artwork.rb
@@ -0,0 +1,13 @@
+class Artwork < ApplicationRecord
+    validates :title, :artist_id
+        presence: true,
+        uniqueness: true
+
+    validates :image_url,
+        presence: true,
+        uniqueness: :true
+
+    belongs_to :user,
+        foreign_key: :artist_id,
+        class_name: :User
+end
