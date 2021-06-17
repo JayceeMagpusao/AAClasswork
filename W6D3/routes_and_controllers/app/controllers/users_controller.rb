9152515f65c1805f3572a06ba01976fc1f4a5a7d
Jenny Nhan <j3nnynhan@gmail.com>
2021-06-16 17:40:01 -0700

    oalmost done! worked on show, update

--- W6D3/routes_and_controllers/app/controllers/users_controller.rb
+++ W6D3/routes_and_controllers/app/controllers/users_controller.rb
@@ -1,23 +1,45 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all
+    end
+    
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save!
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
-    def create
-        user = User.new(user_params)
-    if user.save!
-        render json: user
-    else
-        render json: user.errors.full_messages, status: :unprocessable_entity
-    end
-    end
+    # def create
+    #     user = User.new(user_params)
+    #     if user.save
+    #         render json: user
+    #     else
+    #         render json: user.errors.full_messages, status: :unprocessable_entity
+    #     end
+    # end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+		render json: @user
     end
 
-    def user_params
-        params.require(:user).permit(:name, :email)
+    def update
+        @user = User.find(params[:id])
+        if @user.update(params.require(:user).permit(:name, :email))
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: :422
+        end
     end
+
+
+    
+
+    # def user_params
+    #     params.require(:user).permit(:name, :email)
+    # end
 
 end
