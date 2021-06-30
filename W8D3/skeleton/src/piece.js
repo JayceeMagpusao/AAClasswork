eb63b2ef3ef3fa1448ae7c8d157bb54df54898b6
lbrilli <lbrilli11@gmail.com>
2021-06-30 15:23:36 -0600

    finish piece class

--- W8D3/skeleton/src/piece.js
+++ W8D3/skeleton/src/piece.js
@@ -2,25 +2,29 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    return this.color === "black" ? "white" : "black";
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    this.color = this.oppColor();
 };
 
 /**
  * Returns a string representation of the string
- * based on its color.
+ * based on its color. (returns something we can print on the board)
  */
 Piece.prototype.toString = function () {
+    return this.color === "black" ? "B" : "W";
 };
 
 // DON'T TOUCH THIS CODE
