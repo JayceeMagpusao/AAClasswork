ff10b69796150e80aebcac1f93ec605222871b91
erikingebretson <erikingebretson@gmail.com>
2021-07-16 16:55:31 -0700

    working on tick's setInterval

--- W9D5/skeleton/frontend/clock.jsx
+++ W9D5/skeleton/frontend/clock.jsx
@@ -3,13 +3,30 @@ import React from 'react';
 class Clock extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      time: new Date()
+    }
+    this.tick = this.tick.bind(this)
+  }
+  
+  tick() {
+    let second = this.state.time
+    this.setState({time: second + 1})
   }
 
-  render () {
-  
-    return (<h1>Hello World</h1>)
+  componentDidMount() {
+    setInterval(this.tick, 1000)
   }
-}
+  
+  render () {
+    const hours = this.state.time.getHours()
+    const minutes = this.state.time.getMinutes()
+    const seconds = this.state.time.getSeconds()
+    return (
+      <h1>{hours}:{minutes}:{seconds}</h1>
+      )
+    }
+  }
 
 
 export default Clock;
