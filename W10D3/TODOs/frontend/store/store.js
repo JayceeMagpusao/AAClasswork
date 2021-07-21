5ad928dbfe38e1862e05d82979f6ca792659c192
jaycee <jay27mag@yahoo.com>
2021-07-20 18:21:05 -0700

    cleaned up some code

--- W10D3/TODOs/frontend/store/store.js
+++ W10D3/TODOs/frontend/store/store.js
@@ -2,6 +2,7 @@ import { createStore } from "redux";
 import rootReducer from "../reducers/root_reducer";
 
 const configureStore = (preloadedState = {}) => {
-    return createStore(rootReducer, preloadedState);
+  return createStore(rootReducer, preloadedState);
 }
+
 export default configureStore;
